db9af370721fed4a46b969776e7840c24fc893a3
bs2896-stack <bs2896@est.ort.edu.ar>
2025-10-16 12:03:11 -0300

    avancé

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import {
+
+    subscribeGETEvent, subscribePOSTEvent, startserver
+
+}  from "soquetic";
+
+import fs from "fs";
+
+function enviarSabores(){
+    const sabores = JSON.parse(fs.readFileSync("data/sabores.json" , "utf-8"));
+    return sabores;
+
+}
+
+function enviarProductos(){
+    const productos = JSON.parse(fs.readFileSync("data/productos.json" , "utf-8"));
+    return productos;
+    
+
+}
+
+
+function enviarPedido(producto, sabores, nombre) {
+    let pedidos= JSON.parse(fs.readFileSync("data/pedidos.json", "utf-8"));
+
+if (!Array.isArray(pedidos)) pedidos = [];
+
+let Pedido = (producto; producto, sabores;  sabores, nombre; nombre);
+pedidos.push(Pedido);
+let pedidosJSON= JSON.stringify(pedidos, null, 2)
+fs.writeFileSync("data/pedidos.json", pedidosJSON);
+return {ok:true};
+
+
+
+
+}
